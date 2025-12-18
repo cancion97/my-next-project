5933f440f84ca9c9a5ac34131ba311b8fc62b11e
cancion97 <mikiasm1207@gmail.com>
2025-12-18 10:50:11 +0900

    下書きプレビュー時はSSRする

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -39,22 +39,20 @@ const client = createClient({
 });
 
 export const getMemberList = async (queries?: MicroCMSQueries) => {
-  const listData = await client
-    .getList<Member>({
-      endpoint: "members",
-      queries,
-    });
+  const listData = await client.getList<Member>({
+    endpoint: "members",
+    queries,
+  });
   return listData;
-}
+};
 
 export const getNewsList = async (queries?: MicroCMSQueries) => {
-  const listData = await client
-    .getList<News>({
-      endpoint: "news",
-      queries,
-    });
+  const listData = await client.getList<News>({
+    endpoint: "news",
+    queries,
+  });
   return listData;
-}
+};
 
 export const getNewsDetail = async (
   contentId: string,
@@ -64,9 +62,14 @@ export const getNewsDetail = async (
     endpoint: "news",
     contentId,
     queries,
+    customRequestInit: {
+      next: {
+        revalidate: queries?.draftKey === undefined ? 60 : 0,
+      },
+    },
   });
   return detailData;
-}
+};
 
 export const getCateoryDetail = async (
   contentId: string,
@@ -78,4 +81,4 @@ export const getCateoryDetail = async (
     queries,
   });
   return detailData;
-}
+};
